238efae1fd8e231e18ddf0d8a1eeb512b185acef
geeyana <gpnicomedes@gmail.com>
2026-04-06 21:41:45 -0700

    about page mostly done. main menu not styled.

---
+++ src/main/java/org/example/cs153assignment5/AboutController.java
@@ -0,0 +1,21 @@
+package org.example.cs153assignment5;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import java.io.IOException;
+
+public class AboutController {
+    @FXML
+    private void onExitClick() {
+        Platform.exit();
+    }
+
+    @FXML
+    private void onBackClick() {
+        try {
+            HelloApplication.switchScene("hello-view.fxml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/org/example/cs153assignment5/HelloApplication.java
+++ src/main/java/org/example/cs153assignment5/HelloApplication.java
@@ -8,12 +8,29 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game-page.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+    private static Stage primaryStage;
+    
+
+@Override
+public void start(Stage stage) throws IOException {
+    primaryStage = stage;
+    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(), 600, 400); 
+    scene.getStylesheets().add(
+        HelloApplication.class.getResource("style.css").toExternalForm()
+    );
+    stage.setTitle("Rock Paper Scissors");
+    stage.setResizable(false);
+    stage.setScene(scene);
+    stage.show();
+}
+
+public static void switchScene(String fxmlFile) throws IOException {
+    FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource(fxmlFile));
+    Scene scene = new Scene(loader.load(), 600, 400); 
+    scene.getStylesheets().add(
+        HelloApplication.class.getResource("style.css").toExternalForm()
+    );
+    primaryStage.setScene(scene);
+}
 }
